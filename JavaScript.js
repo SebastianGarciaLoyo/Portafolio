8b0817bc91e3afe51a04e4250ed2445531e6f728
SebastianGarciaLoyo <est.sloyo@smart.edu.co>
2024-08-20 16:18:44 -0500

    feat: :art: Nuevo

--- JavaScript.js
+++ JavaScript.js
@@ -8,3 +8,18 @@ function mostrarSeccion(id) {
     // Mostrar la sección con el ID correspondiente
     document.getElementById(id).classList.add('active');
 }
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    let body = document.getElementById("cuerpo");
+    let button = document.createElement("button");
+    let buttontext = document.createTextNode("Click here if you want background music");
+    button.setAttribute("id", "musica");
+    button.setAttribute("class", "musica");
+    button.appendChild(buttontext);
+    body.appendChild(button);
+
+    document.getElementById("musica").addEventListener("click", (e) => {
+        let sonido = new Audio ("./imagenes/Y2meta.app-Calm Night __ Decaying winter soug new loop __.ogg");
+        sonido.play();
+    })
+})
